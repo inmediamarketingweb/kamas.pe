a67d490060e3d90cecea19a50b67047f79675801
Inmedia 11 <inmedia.pc.11@gmail.com>
2026-06-20 10:55:53 -0500

    2.0.0

--- src/Componentes/Header/Componentes/Bottom/Bottom.js
+++ src/Componentes/Header/Componentes/Bottom/Bottom.js
@@ -4,8 +4,8 @@ function Bottom(){
     return(
         <div className='header-bottom-container'>
             <section className='header-bottom'>
-                <p className='d-flex-center-center'><a href='/productos/dormitorios/?tamaño=king' title='Hasta -30% de descuento en todo dormitorios king 🛌' className='color-white'>Hasta -30% de descuento en todo dormitorios king 🛌</a></p>
-                <p className='d-flex-center-center'><a href='/productos/dormitorios/?tamaño=king' title='-30% en dormitorios king 🛌' className='color-white'>-30% en dormitorios king 🛌</a></p>
+                <p className='d-flex-center-center text-center visible-on-desktop-no-mobile'><a href='/productos/dormitorios/?tamaño=king' title='Hasta -30% de descuento en todo dormitorios king 🛌' className='color-white'>Hasta -30% de descuento en todo dormitorios king 🛌</a></p>
+                <p className='w-100 d-flex-center-center text-center visible-on-mobile-no-desktop'><a href='/productos/dormitorios/?tamaño=king' title='-30% en dormitorios king 🛌' className='color-white'>-30% en dormitorios king 🛌</a></p>
 
                 <ul className='d-flex-center-center gap-10'>
                     <li>
